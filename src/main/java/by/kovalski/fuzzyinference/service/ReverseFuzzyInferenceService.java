d9dd350b83d03eb735820be4bfcdf6f76bdeff4b
Daniil <bilkoudan@gmail.com>
2024-12-11 05:14:22 +0300

    merged feat/kb-parser

--- src/main/java/by/kovalski/fuzzyinference/service/ReverseFuzzyInferenceService.java
+++
@@ -1,9 +0,0 @@
-//package by.kovalski.fuzzyinference.service;
-//
-//import by.kovalski.fuzzyinference.entity.BinaryFuzzyPredicate;
-//import by.kovalski.fuzzyinference.entity.FuzzySet;
-//import by.kovalski.fuzzyinference.entity.ReverseFuzzyInference;
-//
-//public interface ReverseFuzzyInferenceService {
-//    ReverseFuzzyInference makeInference(BinaryFuzzyPredicate implication, FuzzySet ySet);
-//}
